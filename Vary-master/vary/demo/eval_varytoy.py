5a086e0d7a0b501406aa1ee5a8c6470aa21b6081
王振 <firebux@intsig.net>
2024-05-10 18:09:35 +0800

    update

--- Vary-master/vary/demo/eval_varytoy.py
+++ Vary-master/vary/demo/eval_varytoy.py
@@ -112,7 +112,7 @@ def eval_model(args):
             input_ids,
             images=[(image_tensor.unsqueeze(0).half().cuda(), image_tensor_1.unsqueeze(0).half().cuda())],
             do_sample=True,
-            num_beams=3,
+            num_beams=1,
             temperature=1,
             streamer=streamer,
             max_new_tokens=2048,
